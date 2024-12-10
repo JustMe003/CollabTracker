67a41dc2fa2b712a5cbd952ede1d88489f16e396
JustMe003 <kingoflol20033@gmail.com>
2024-12-10 16:55:27 +0100

    Cleaned writers
    Co-Authored-By: R-Selaru <127208596+R-Selaru@users.noreply.github.com>

--- Github CollabTracker/src/Writers/Writer.ts
+++ Github CollabTracker/src/Writers/Writer.ts
@@ -3,31 +3,6 @@ import * as fs from "@tauri-apps/plugin-fs";
 export abstract class Writer {
   protected path: string = "";
 
-  // Initialize the Writer by checking and creating the folder/file
-  public async init(path:string) {
-    this.path = path
-    const exists = await this.checkFile();
-    if (!exists) {
-      await this.createFile();
-    }
-  }
-
-  // Check if the file/folder exists
-  protected async checkFile(): Promise<boolean> {
-    return await fs.exists(this.path, {baseDir: fs.BaseDirectory.AppLocalData});
-  }
-
-  // Create the folder/file if it doesn't exist
-  protected async createFile() {
-    try {
-      await fs.create(this.path, {
-        baseDir: fs.BaseDirectory.AppLocalData,
-      });
-      console.log(`File created at: ${this.path}`);
-    } catch (error) {
-      console.error('Error creating folder:', error);
-    }
-  }
 
   async writeObject(object: object) {
     try {
