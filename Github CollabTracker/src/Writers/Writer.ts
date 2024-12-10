38dacd798c367090e1da3bba62d5516b6a871095
JustMe003 <kingoflol20033@gmail.com>
2024-12-10 16:08:53 +0100

    Seperated array and object writing

--- Github CollabTracker/src/Writers/Writer.ts
+++ Github CollabTracker/src/Writers/Writer.ts
@@ -40,12 +40,30 @@ export abstract class Writer {
       } else {
         jsonObject = {};
       }
-      console.log("Read contents" + jsonObject)
       Object.assign(jsonObject, object);
       await fs.writeTextFile(this.path, JSON.stringify(jsonObject), {
         baseDir: fs.BaseDirectory.AppLocalData,
       })
-      console.log("File updated successfully!");
+    } catch (error) {
+      console.error("Error updating file:", error);
+    }
+  }
+
+  async writeObjects(object: object[]) {
+    try {
+      const fileContents = await fs.readTextFile(this.path, {
+        baseDir: fs.BaseDirectory.AppLocalData,
+      });
+      let jsonObject;
+      if (fileContents) {
+        jsonObject = JSON.parse(fileContents);
+      } else {
+        jsonObject = [];
+      }
+      Object.assign(jsonObject, object);
+      await fs.writeTextFile(this.path, JSON.stringify(jsonObject), {
+        baseDir: fs.BaseDirectory.AppLocalData,
+      })
     } catch (error) {
       console.error("Error updating file:", error);
     }
