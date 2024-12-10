e0550c00bba692da8872de09e75878e941b9b768
JustMe003 <kingoflol20033@gmail.com>
2024-12-10 11:50:52 +0100

    Update DataManager.ts

--- Github CollabTracker/src/DataManager/DataManager.ts
+++ Github CollabTracker/src/DataManager/DataManager.ts
@@ -1,9 +1,18 @@
+import { Scraper } from "../ApiScraper/Scraper";
 import { RepoModel } from "../Models/RepoModel";
+import { UserModel } from "../Models/UserModel";
 
 export class DataManager {
-  private repos: RepoModel;
+  private scraper: Scraper;
+  private repos: RepoModel | null = null;
+  private us: UserModel | null = null;
 
   constructor(token: string) {
-    
+    this.scraper = new Scraper(token);
+    this.getUserData();
+  }
+
+  private async getUserData(): Promise<void> {
+    this.us = await this.scraper.scrapeUser();
   }
 }
