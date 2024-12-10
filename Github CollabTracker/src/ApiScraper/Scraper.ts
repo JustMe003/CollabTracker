c9f00ea01c67fa86b145cdcde7845964d18521a8
Robert Selaru <r.selaru@student.rug.nl>
2024-12-10 11:36:25 +0100

    user scraper

--- Github CollabTracker/src/ApiScraper/Scraper.ts
+++ Github CollabTracker/src/ApiScraper/Scraper.ts
@@ -1,3 +1,14 @@
-export abstract class Scraper {
+import { UserApiModel } from "../ApiModels/UserApiModel";
+import { GitTokenCookie } from "../authorization/cookies/GitTokenCookie";
+import { RequestGithub } from "../requestGithub";
+
+export abstract class Scraper{
+  public static async scrapeUser() : Promise<UserApiModel>{
+    const userData =  await RequestGithub.sendGetRequest(
+      "https://api.github.com/user",
+      new Map<string, string>(),
+      GitTokenCookie.getGitTokenCookie() as string) as UserApiModel
+    return userData;
+  }
   
 }
