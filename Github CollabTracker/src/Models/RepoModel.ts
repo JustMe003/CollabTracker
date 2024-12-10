56603764f988ac5652b4f603a6590f8db151483b
JustMe003 <kingoflol20033@gmail.com>
2024-12-10 11:32:18 +0100

    Updated Repo models and created data manager class

--- Github CollabTracker/src/Models/RepoModel.ts
+++ Github CollabTracker/src/Models/RepoModel.ts
@@ -1,13 +1,36 @@
+import { BranchModel } from "./BranchModel";
 import {UserModel} from "./UserModel"
 
 export class RepoModel{
-  public repoID: string;
-  public html: string;
-  public creator: UserModel;
+  private repoID: string;
+  private html: string;
+  private creator: UserModel;
+  private branches: BranchModel[] = []
+
 
   constructor(repoID: string, html: string, creator: UserModel){
     this.repoID = repoID;
     this.html = html;
     this.creator = creator;
+  }
+
+  public getRepoID() {
+    return this.repoID;
+  }
+
+  public getHtml() {
+    return this.html;
+  }
+
+  public getCreator() {
+    return this.creator;
+  }
+
+  public getBranches() {
+    return this.branches;
+  }
+
+  public setBranches(branches: BranchModel[]) {
+    this.branches = branches;
   }
 }
