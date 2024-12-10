5112bee4701e4dc8080a9e88611a33dfee1bc0f8
Robert Selaru <r.selaru@student.rug.nl>
2024-12-10 11:52:18 +0100

    scrapers

--- Github CollabTracker/src/ApiScraper/Scraper.ts
+++ Github CollabTracker/src/ApiScraper/Scraper.ts
@@ -1,3 +1,5 @@
+import { BranchApiModel } from "../ApiModels/BranchApiModel";
+import { RepoApiModel } from "../ApiModels/RepoApiModel";
 import { UserApiModel } from "../ApiModels/UserApiModel";
 import { GitTokenCookie } from "../authorization/cookies/GitTokenCookie";
 import { RequestGithub } from "../requestGithub";
@@ -9,6 +11,22 @@ export abstract class Scraper{
       new Map<string, string>(),
       GitTokenCookie.getGitTokenCookie() as string) as UserApiModel
     return userData;
+  }
+
+  public static async scrapeRepos() : Promise<RepoApiModel[]>{
+    const repos =  await RequestGithub.sendGetRequest(
+      "https://api.github.com/user/repos",
+      new Map<string, string>(),
+      GitTokenCookie.getGitTokenCookie() as string) as RepoApiModel[]
+    return repos;
+  }
+
+  public static async scrapeBranches(repoID: string) : Promise<BranchApiModel[]>{
+    const branches =  await RequestGithub.sendGetRequest(
+      "https://api.github.com/repositories/${repoID}/branches",
+      new Map<string, string>(),
+      GitTokenCookie.getGitTokenCookie() as string) as BranchApiModel[]
+    return branches;
   }
   
 }
