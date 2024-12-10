7b91a14000b9b781eadee07abaf1109459a1d87a
Robert Selaru <r.selaru@student.rug.nl>
2024-12-10 12:24:04 +0100

    changes to models

--- Github CollabTracker/src/Models/BranchModel.ts
+++ Github CollabTracker/src/Models/BranchModel.ts
@@ -1,11 +1,18 @@
+import { BranchApiModel } from "../ApiModels/BranchApiModel";
+import { UserApiModel } from "../ApiModels/UserApiModel";
 import { UserModel } from "./UserModel";
 
 export class BranchModel{
   public creator: UserModel;
   public sha: string;
   
-  constructor(creator: UserModel, sha: string){
-    this.creator = creator;
-    this.sha = sha;
+  constructor(apiModel: BranchApiModel){
+    this.creator = new UserModel(apiModel.creator);
+    this.sha = apiModel.sha;
+  }
+
+  public static createNew(creator: UserModel, sha: string){
+    return new BranchModel({creator: {name: creator.username, html_url:creator.html} as UserApiModel, sha:sha} as BranchApiModel);
+
   }
 }
