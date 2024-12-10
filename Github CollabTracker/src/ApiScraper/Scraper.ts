f732bc285936004bbeead4819028ba40c910dcff
JustMe003 <kingoflol20033@gmail.com>
2024-12-10 11:42:11 +0100

    Update Scraper.ts

--- Github CollabTracker/src/ApiScraper/Scraper.ts
+++ Github CollabTracker/src/ApiScraper/Scraper.ts
@@ -1,14 +1,18 @@
 import { UserApiModel } from "../ApiModels/UserApiModel";
-import { GitTokenCookie } from "../authorization/cookies/GitTokenCookie";
 import { RequestGithub } from "../requestGithub";
 
-export abstract class Scraper{
-  public static async scrapeUser() : Promise<UserApiModel>{
+export class Scraper {
+  private token: string;
+
+  constructor(token: string) {
+    this.token = token;
+  }
+
+  public async scrapeUser() : Promise<UserApiModel>{
     const userData =  await RequestGithub.sendGetRequest(
       "https://api.github.com/user",
       new Map<string, string>(),
-      GitTokenCookie.getGitTokenCookie() as string) as UserApiModel
+      this.token) as UserApiModel
     return userData;
   }
-  
 }
