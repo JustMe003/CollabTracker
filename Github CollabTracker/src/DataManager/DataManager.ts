54ec3d0f60b5e72de58c755feacecc92eee15bf1
JustMe003 <kingoflol20033@gmail.com>
2024-12-10 16:10:52 +0100

    Restart on DataManager
    Co-Authored-By: R-Selaru <127208596+R-Selaru@users.noreply.github.com>

--- Github CollabTracker/src/DataManager/DataManager.ts
+++ Github CollabTracker/src/DataManager/DataManager.ts
@@ -1,37 +1,3 @@
-import { Scraper } from "../ApiScraper/Scraper";
-import { RepoModel, UserModel } from "../Models";
-import { WriteHandler } from "../Writers/WriteHandler";
-
 export class DataManager {
-  private scraper: Scraper;
-  private repos: RepoModel[] = [];
-  private us: UserModel | null = null;
-
-  constructor(token: string) {
-    this.scraper = new Scraper(token);
-    this.getUserData().finally(() => {
-      console.log(this.us);
-      const wh = new WriteHandler(this.us?.getUserName() ?? "test");
-      wh.init().then(() => {
-        wh.writeUser(this.us ?? {});
-      });
-    });
-    this.getAllRepos().finally(() => {
-      const wh = new WriteHandler(this.us?.getUserName() ?? "test");
-      console.log(this.repos);
-      wh.init().then(() => {
-        wh.writeRepos(this.repos);
-      })
-    });
-  }
-
-  private async getUserData(): Promise<void> {
-    this.us = new UserModel(await this.scraper.scrapeUser());
-  }
-
-  private async getAllRepos(): Promise<void> {
-    (await this.scraper.scrapeRepos()).forEach((m) => {
-      this.repos.push(new RepoModel(m));
-    });
-  }
+  
 }
