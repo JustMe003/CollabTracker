ca9037a5c0c8910720d77b5d783de6e46fcbc8ed
JustMe003 <kingoflol20033@gmail.com>
2024-12-10 16:55:43 +0100

    Started on main page
    Co-Authored-By: R-Selaru <127208596+R-Selaru@users.noreply.github.com>

--- Github CollabTracker/src/pages/main/mainPage.ts
+++ Github CollabTracker/src/pages/main/mainPage.ts
@@ -1,10 +1,19 @@
 import { GitRefreshCookie } from "../../authorization/cookies/GitRefreshCookie";
 import { GitTokenCookie } from "../../authorization/cookies/GitTokenCookie";
 import { DataManager } from "../../DataManager/DataManager";
+import { FileStorage } from "../../FileIO/FileStorage";
 import router from "../../router/router";
 
-export function test() {
-  // const dataManager = new DataManager(GitTokenCookie.getGitTokenCookie() as string);
+export async function test() {
+  const storage = new FileStorage();
+  await storage.init();
+  const token = GitTokenCookie.getGitTokenCookie();
+  if (token) {
+    const manager = new DataManager(token);
+    const us = await manager.getUser();
+    storage.initUserStorage(us.getUserName());
+    // Now we have storage yay :)
+  }
 }
   
 export function logOut() {
