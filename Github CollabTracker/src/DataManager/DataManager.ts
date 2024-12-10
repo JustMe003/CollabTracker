da237f72c7c6f9ce543cf07a6e90af2f3a67a227
JustMe003 <kingoflol20033@gmail.com>
2024-12-10 16:55:54 +0100

    Started on DataManager
    Co-Authored-By: R-Selaru <127208596+R-Selaru@users.noreply.github.com>

--- Github CollabTracker/src/DataManager/DataManager.ts
+++ Github CollabTracker/src/DataManager/DataManager.ts
@@ -1,3 +1,16 @@
+import { Scraper } from "../ApiScraper/Scraper";
+import { UserModel } from "../Models";
+
 export class DataManager {
-  
+  private scraper: Scraper;
+  // private reader: Reader;
+  // private writer: Writer;
+
+  constructor(token: string) {
+    this.scraper = new Scraper(token);
+  }
+
+  public async getUser(): Promise<UserModel> {
+    return new UserModel(await this.scraper.scrapeUser());
+  }
 }
