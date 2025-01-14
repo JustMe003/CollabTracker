6e49d78c6b697032460afb36cea69ef6876d5695
Robert Selaru <r.selaru@student.rug.nl>
2025-01-14 15:19:30 +0100

    deleted updated_at repo model

--- Github CollabTracker/src/ApiModels/RepoApiModel.ts
+++ Github CollabTracker/src/ApiModels/RepoApiModel.ts
@@ -3,7 +3,6 @@ import {UserApiModel} from "./UserApiModel"
 export interface RepoApiModel{
   id: number;
   html_url: string;
-  updated_at: Date;
   owner: UserApiModel;
   name: string;
 }
--- Github CollabTracker/src/ModelConverter/RepoModelConverter.ts
+++ Github CollabTracker/src/ModelConverter/RepoModelConverter.ts
@@ -3,6 +3,6 @@ import { BranchObject, IssueModel, IssueObject, RepoModel } from "../Models";
 
 export class RepoModelConverter {
   public static convert(apiModel: RepoApiModel, branches: BranchObject = {}, issues: IssueObject = {}, pullReqs: Map<number, IssueModel> = new Map()): RepoModel {
-    return new RepoModel(apiModel.id, apiModel.name, apiModel.html_url, apiModel.owner.id, apiModel.updated_at, branches, issues, pullReqs, []);
+    return new RepoModel(apiModel.id, apiModel.name, apiModel.html_url, apiModel.owner.id, branches, issues, pullReqs, []);
   }
 }
