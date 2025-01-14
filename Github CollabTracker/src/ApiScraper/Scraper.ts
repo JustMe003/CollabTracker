2b4b65c8ad4314175f99270ad997c50bcff9e995
Robert Selaru <r.selaru@student.rug.nl>
2025-01-14 13:55:14 +0100

    scraper

--- Github CollabTracker/src/ApiScraper/Scraper.ts
+++ Github CollabTracker/src/ApiScraper/Scraper.ts
@@ -36,42 +36,84 @@ export class Scraper {
 
 
   public async scrapeIssues() : Promise<apiMod.IssueApiModel[]>{
-    try {
-      const issues = await RequestGithub.sendGetRequest(
+    let page = 1;
+    let issues: apiMod.IssueApiModel[] = [];
+    do {
+
+      issues = issues.concat(await RequestGithub.sendGetRequest(
         "https://api.github.com/user/issues",
         new Map<string, string>([
           ["filter", "all"],
-          ["state", "all"]
+          ["state", "all"],
+          ["sort", "updated"],
+          ["per_page", Scraper.perPage.toString()],
+          ["page", page.toString()]
         ]),
-        this.token) as apiMod.IssueApiModel[]
+        this.token) as apiMod.IssueApiModel[]);
+      } while (issues.length / Scraper.perPage >= page++);
       return issues;
-    } catch(e) {
-      console.log(e);
     }
-    return [];
-  }
-
+    
   public async scrapePullRequests(username: string) : Promise<apiMod.IssueApiModel[]>{
-    const prs = await RequestGithub.sendGetRequest(
-      "https://api.github.com/repos/{owner}/{repo}/pulls",
-      new Map<string, string>([
-        ["involves", username],
-        ["is", "pr"]
-      ]),
-      this.token) as apiMod.IssueApiModel[]
-    return prs;
-  }
-
-
+    let page = 1;
+    let pullRequests: apiMod.IssueApiModel[] = [];
+    do {
+      pullRequests = pullRequests.concat(await RequestGithub.sendGetRequest(
+        "https://api.github.com/repos/{owner}/{repo}/pulls",
+        new Map<string, string>([
+          ["involves", username],
+          ["is", "pr"],
+          ["sort", "updated"],
+          ["per_page", Scraper.perPage.toString()],
+          ["page", page.toString()]
+        ]),
+        this.token) as apiMod.IssueApiModel[]);
+      } while (pullRequests.length / Scraper.perPage >= page++);
+      return pullRequests;
+    }
+      
+      
   public async scrapeBranches(owner:string, repoID: string): Promise<apiMod.BranchApiModel[]> {
-    const branches = await RequestGithub.sendGetRequest(
+    let page = 1;
+    let branches: apiMod.BranchApiModel[] = [];
+    do {
+      branches = branches.concat(await RequestGithub.sendGetRequest(
         `https://api.github.com/repos/${owner}/${repoID}/branches`,
-        new Map<string, string>(),
-        this.token
-    ) as apiMod.BranchApiModel[];
+        new Map<string, string>([
+          ["per_page", Scraper.perPage.toString()],
+          ["page", page.toString()]
+        ]),
+        this.token) as apiMod.BranchApiModel[]);
+    } while (branches.length / Scraper.perPage >= page++);
     return branches;
   }
 
-
+  public async scrapeCommits(owner:string, repoID: string, nameBranch:string, last_updated: Date | undefined): Promise<apiMod.CommitsApiModel[]> {
+    let page = 1;
+    let commits: apiMod.CommitsApiModel[] = [];
+    do {
+      if(last_updated == undefined) {
+        commits = commits.concat(await RequestGithub.sendGetRequest(
+          `https://api.github.com/repos/${owner}/${repoID}/commits`,
+          new Map<string, string>([
+            ["sha", nameBranch],
+            ["per_page", Scraper.perPage.toString()],
+            ["page", page.toString()]
+          ]),
+          this.token) as apiMod.CommitsApiModel[]);
+      } else {
+        commits = commits.concat(await RequestGithub.sendGetRequest(
+          `https://api.github.com/repos/${owner}/${repoID}/commits`,
+          new Map<string, string>([
+            ["sha", nameBranch],
+            ["since", last_updated.toDateString()],
+            ["per_page", Scraper.perPage.toString()],
+            ["page", page.toString()]
+          ]),
+          this.token) as apiMod.CommitsApiModel[]);
+      }
+    } while (commits.length / Scraper.perPage >= page++);
+    return commits;
+  }
 
 }
